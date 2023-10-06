1be34127c52739d8982e1afe232c9293cb914c85
Martet <martin@zmitko.cz>
2023-10-06 11:07:02 +0200

    BFS

--- sui-solution.cc
+++ sui-solution.cc
@@ -1,6 +1,32 @@
+#include <algorithm>
+#include <queue>
+#include <set>
+
 #include "search-strategies.h"
 
 std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_state) {
+	std::queue<std::pair<SearchState, std::vector<SearchAction>>> open;
+	std::set<SearchState> closed;
+	open.push(std::pair(init_state, std::vector<SearchAction>()));
+
+	while (!open.empty()) {
+		auto state = open.front();
+		open.pop();
+		if (state.first.isFinal())
+			return state.second;
+		auto insert = closed.insert(state.first);
+		if (!insert.second)
+			continue;
+
+		auto actions = state.first.actions();
+		if (actions.size() == 0)
+			continue;
+		for (auto action: actions) {
+			auto new_actions(state.second);
+			new_actions.push_back(action);
+			open.push(std::pair(action.execute(state.first), new_actions));
+		}
+	}
 	return {};
 }
 
