309dc9b1704bd5ecf3bbd4acd7156bc2494b2353
Martet <martin@zmitko.cz>
2023-10-06 14:43:23 +0200

    A* with saving all paths

--- sui-solution.cc
+++ sui-solution.cc
@@ -65,5 +65,38 @@ double StudentHeuristic::distanceLowerBound(const GameState &state) const {
 }
 
 std::vector<SearchAction> AStarSearch::solve(const SearchState &init_state) {
+	auto compare_fn = [](const std::pair<double, std::pair<SearchState, std::vector<SearchAction>>> &a, const std::pair<double, std::pair<SearchState, std::vector<SearchAction>>> &b) {
+		return a.first > b.first;
+	};
+	std::priority_queue<std::pair<double, std::pair<SearchState, std::vector<SearchAction>>>, std::vector<std::pair<double, std::pair<SearchState, std::vector<SearchAction>>>>, decltype(compare_fn)> open(compare_fn);
+	std::set<SearchState> closed;
+	std::map<SearchState, int> g;
+	open.push(std::pair(0, std::pair(init_state, std::vector<SearchAction>())));
+	while (!open.empty()) {
+		auto top_f = open.top().first;
+		auto state = open.top().second;
+		open.pop();
+		if (state.first.isFinal())
+			return state.second;
+		auto insert = closed.insert(state.first);
+		if (!insert.second)
+			continue;
+
+		auto actions = state.first.actions();
+		if (actions.size() == 0)
+			continue;
+		for (auto action: actions) {
+			auto new_state = action.execute(state.first);
+			int new_g = g[state.first] + 1;
+			double f = new_g + compute_heuristic(new_state, *heuristic_);
+			if (g.count(new_state))
+				if (new_g < g[new_state] || (new_g == g[new_state] && f < top_f))
+					continue;
+			g[new_state] = new_g;
+			auto new_actions(state.second);
+			new_actions.push_back(action);
+			open.push(std::pair(f, std::pair(new_state, new_actions)));
+		}
+	}
 	return {};
 }
