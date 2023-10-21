92d077e9ff69956a52c7714dbdb712dd973f8aa6
Martet <martin@zmitko.cz>
2023-10-21 18:32:20 +0200

    A* save path as tree

--- sui-solution.cc
+++ sui-solution.cc
@@ -63,36 +63,54 @@ double StudentHeuristic::distanceLowerBound(const GameState &state) const {
     return 0;
 }
 
+struct SearchNode {
+	int g;
+	std::shared_ptr<SearchNode> parent;
+	std::shared_ptr<SearchAction> action;
+};
+
+std::vector<SearchAction> reconstruct_path(std::shared_ptr<SearchNode> node) {
+	std::vector<SearchAction> path;
+	while (node->action) {
+		path.push_back(*(node->action));
+		node = node->parent;
+	}
+	std::reverse(path.begin(), path.end());
+	return path;
+}
+
 std::vector<SearchAction> AStarSearch::solve(const SearchState &init_state) {
-	auto compare_fn = [](const std::pair<double, std::pair<SearchState, std::vector<SearchAction>>> &a, const std::pair<double, std::pair<SearchState, std::vector<SearchAction>>> &b) {
+	auto compare_fn = [](const std::pair<double, SearchState> &a, const std::pair<double, SearchState> &b) {
 		return a.first > b.first;
 	};
-	std::priority_queue<std::pair<double, std::pair<SearchState, std::vector<SearchAction>>>, std::vector<std::pair<double, std::pair<SearchState, std::vector<SearchAction>>>>, decltype(compare_fn)> open(compare_fn);
+	std::priority_queue<std::pair<double, SearchState>, std::deque<std::pair<double, SearchState>>, decltype(compare_fn)> open(compare_fn);
 	std::set<SearchState> closed;
-	std::map<SearchState, int> g;
-	open.push(std::pair(0, std::pair(init_state, std::vector<SearchAction>())));
+	std::map<SearchState, SearchNode> nodes;
+	nodes[init_state] = {0, nullptr, nullptr};
+	open.push(std::pair(0, init_state));
 	while (!open.empty()) {
 		auto top_f = open.top().first;
 		auto state = open.top().second;
 		open.pop();
-		if (state.first.isFinal())
-			return state.second;
-		auto insert = closed.insert(state.first);
+		auto insert = closed.insert(state);
 		if (!insert.second)
 			continue;
 
-		auto actions = state.first.actions();
+		int new_g = nodes[state].g + 1;
+		auto actions = state.actions();
 		for (auto action: actions) {
-			auto new_state = action.execute(state.first);
-			int new_g = g[state.first] + 1;
+			auto new_state = action.execute(state);
 			double f = new_g + compute_heuristic(new_state, *heuristic_);
-			if (g.count(new_state))
-				if (new_g < g[new_state] || (new_g == g[new_state] && f < top_f))
+			struct SearchNode node = {new_g, std::make_shared<SearchNode>(nodes[state]), std::make_shared<SearchAction>(action)};
+			if (nodes.count(new_state))
+				if (new_g > nodes[new_state].g || (new_g == nodes[new_state].g && f > top_f))
 					continue;
-			g[new_state] = new_g;
-			auto new_actions(state.second);
-			new_actions.push_back(action);
-			open.push(std::pair(f, std::pair(new_state, new_actions)));
+			if (new_state.isFinal()){
+				std::cout << getCurrentRSS() << std::endl;
+				return reconstruct_path(std::make_shared<SearchNode>(node));
+			}
+			nodes[new_state] = node;
+			open.push(std::pair(f, new_state));
 		}
 
 		if (getCurrentRSS() > mem_limit_ - 50 * 1000 * 1000)
